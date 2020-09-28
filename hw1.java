18da271801c8a320002c24ffbe77e10fe365e56a
ctavar02 <72041700+ctavar02@users.noreply.github.com>
2020-09-28 15:08:15 -0400

    Add files via upload

---
+++ hw1.java
@@ -0,0 +1,28 @@
+
+public class hw1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//adding integers
+
+
+int a = 1;
+int b = 2;
+
+System.out.print(a+b);
+
+	//name, email and student id
+
+
+String name = "Christopher Tavarez";
+String email = "ctavar02@nyit.edu";
+int idNumber = 1256523;
+
+	System.out.println("My name is "+name+" and my email is "+email+"Finally, my id number is "+idNumber+".");
+
+	//integer plus 1
+	
+	int up = 10;
+up++;
+System.out.println(up);}}
